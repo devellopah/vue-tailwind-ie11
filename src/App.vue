b6ab07fd1531848b3bd65dc91d2bc8c39b066086
Islam <islamibakaev@gmail.com>
2021-03-18 07:23:33 +0300

    added tailwindcss, synced with vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="bg-blue-100 text-center py-10">
+    <img alt="Vue logo" src="./assets/logo.png" class="mx-auto">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +17,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* https://tailwindcss.com/docs/installation#include-tailwind-in-your-css */
+@import "tailwindcss/tailwind.css";
 </style>
